d5798236d8f566a8e06d034d90a61b390a20072d
bobo <www.bobo>
2022-05-16 22:38:02 +0800

    静态链表Achieved

--- StaicList/StaticList.c
+++ StaicList/StaticList.c
@@ -1,5 +1,8 @@
 #include "StaticList.h"
-
+/**
+ * 为什么将Node[0]作为Header来使用？
+ * 答：主要是插入的时候，header.next指向的必定是node[0];在循环遍历的时候不方便遍历查询，header需要特殊处理！
+*/
 typedef struct _tag_StaticListNode
 {
     unsigned int data;
@@ -22,7 +25,7 @@ StaticList* StaticList_Create(int capacity)
     if(capacity < 0)
         return NULL;
 
-    list = (TStaticList*)malloc(sizeof(TStaticList) + sizeof(TStaticListNode) * capacity);
+    list = (TStaticList*)malloc(sizeof(TStaticList) + sizeof(TStaticListNode) * (capacity+1));
 
     if(list != NULL)
     {
@@ -57,7 +60,7 @@ void StaticList_Clear(StaticList* list)
         int cnt = 0;
         slist->header.data = 0;
         slist->header.next = 0;
-        for(cnt=0; cnt<slist->capacity; cnt++)
+        for(cnt=1; cnt<=slist->capacity; cnt++)
         {
             slist->node[cnt].next = AVAILABLE;
         }        
@@ -82,23 +85,120 @@ int StaticList_Length(StaticList* list)
 //返回最大存储量
 int StaticList_Capacity(StaticList* list)
 {
-    int retCapacity = -1;
     TStaticList* slist = (TStaticList*)list;
+    int retCapacity = -1;
+
     if(slist != NULL)
-    {
         retCapacity = slist->capacity;
-    }
 
     return retCapacity;
 }
 
 
 //插入元素
-int StaticList_Insert(StaticList* list, StaticListNode* node, int pos);
+int StaticList_Insert(StaticList* list, StaticListNode* node, int pos)
+{
+    TStaticList* slist = (TStaticList*)list;
+    int current = 0;
+    int index = 0;
+    int i = 0;
+    int ret = (slist != NULL) && (slist->header.data <= slist->capacity)\
+     && (pos>=0) &&  (node != NULL);  
+        
+    if(ret)
+    { 
+        slist->node[0] = slist->header;
+
+        for(i=0; i<slist->capacity; i++)
+        {
+            if(slist->node[i].next == AVAILABLE)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        slist->node[index].data = (unsigned int)*(unsigned int*)node;
+
+        for(i=0; (i<pos)&&(slist->node[current].next != 0); i++)
+        {
+            current = slist->node[current].next;
+            // printf("Insert-current:%d\n", current);
+        }
+
+        slist->node[index].next = slist->node[current].next;
+        slist->node[current].next = index;
+
+        slist->node[0].data += 1;
+
+        slist->header = slist->node[0];
+
+        // printf("Insert:%d, %d, %d\n", slist->header.data, slist->header.next, current);
+    }     
+
+    return ret; 
+}
+
+
 //获取某一位置的元素
-StaticListNode* StaticList_Get(StaticList* list, int pos);
+StaticListNode* StaticList_Get(StaticList* list, int pos)
+{
+    TStaticList* slist = (TStaticList*)list;
+    static StaticListNode* ret = NULL;
+    int current = 0;
+    int obj = 0;
+    int i = 0;
+
+    if(slist!=NULL && pos>=0 && pos<slist->header.data)
+    {
+        slist->node[0] = slist->header;
+
+        for(i=0; i<pos; i++)
+        {
+            current = slist->node[current].next;
+        }
+
+        obj = slist->node[current].next;
+
+        ret = (StaticListNode*)&(slist->node[obj].data);
+        // printf("Get:%d, %d\n", slist->node[current].data, slist->node[current].next);
+    }
+
+    return ret;
+}
+
+
 //删除某一位置的元素
-StaticListNode* StaticList_Delete(StaticList* list, int pos);
+StaticListNode* StaticList_Delete(StaticList* list, int pos)
+{
+    TStaticList* slist = (TStaticList*)list;
+    static StaticListNode* ret = NULL;
+    int current = 0;
+    int obj = 0;
+    int i = 0; 
+
+    if(slist!=NULL && pos>=0 && pos<slist->header.data)
+    {
+        slist->node[0] = slist->header;
+
+        for(i=0; i<pos; i++)
+        {
+            current = slist->node[current].next;
+        }
+
+        obj = slist->node[current].next;
+        slist->node[current].next = slist->node[obj].next;
+        slist->node[0].data -= 1;
+
+        slist->header = slist->node[0];
+
+        slist->node[obj].next = AVAILABLE;
+
+        ret = (StaticListNode*)&(slist->node[obj].data);
+    }
+
+    return ret;
+}
 
 
 
--- StaicList/main.c
+++ StaicList/main.c
@@ -1,10 +1,57 @@
 #include "StaticList.h"
 
 
-
-
 int main()
 {
+    StaticList* list = StaticList_Create(10);
+    int index = 0;
+    int a=1,b=2,c=3,d=4,e=5,f=6;
+    int len = 0;
+    int* delPtr = NULL;
+    int state = -1;
+
+    len = StaticList_Length(list);
+    printf("Capacity:%d, Len:%d\n", StaticList_Capacity(list), len);
+
+    if(FALSE == StaticList_Insert(list, &f, 0))
+        printf("insert failed\n");
+    if(FALSE == StaticList_Insert(list, &b, 0))
+        printf("insert failed\n");
+    if(FALSE == StaticList_Insert(list, &c, 0))
+        printf("insert failed\n");
+    if(FALSE == StaticList_Insert(list, &d, 0))
+        printf("insert failed\n");
+    if(FALSE == StaticList_Insert(list, &e, 0))
+        printf("insert failed\n");
+    if(FALSE == StaticList_Insert(list, &a, 0))
+        printf("insert failed\n");
+
+    len = StaticList_Length(list);
+    printf("Len:%d\n", len);
+
+    for(index=0; index<len; index++)
+    {
+        int* intPtr = (int*)StaticList_Get(list, index);
+
+        printf("%d\t", *intPtr);
+    }
+    printf("\n");
+
+    delPtr = (int*)StaticList_Delete(list, 3);
+
+    len = StaticList_Length(list);
+    printf("Len:%d\n", len);  
+
+    for(index=0; index<len; index++)
+    {
+        int* intPtr = (int*)StaticList_Get(list, index);
+
+        printf("%d\t", *intPtr);
+    }
+    printf("\n");
+
+    StaticList_Destroy(list);
+
 
     return 0;
 }
